fcca6b123a86e3539b981c217a498de169ec96c4
Isaac Marovitz <isaacryu@icloud.com>
2023-03-23 11:02:54 -0400

    Initial Commit

---
+++ Whisky/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Whisky
+//
+//  Created by Isaac Marovitz on 23/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Whisky/WhiskyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhiskyApp.swift
+//  Whisky
+//
+//  Created by Isaac Marovitz on 23/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct WhiskyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
