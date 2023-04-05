3acb72e606b5abe2f990b31d7a2c41b397d92ed8
Isaac Marovitz <isaacryu@icloud.com>
2023-04-04 20:21:55 -0400

    Found it

--- Whisky/Models/PortableExecutable.swift
+++ Whisky/Models/PortableExecutable.swift
@@ -21,40 +21,22 @@ struct COFFFileHeader: Hashable {
                 throw("File is not a PE")
             }
 
-            offset += 4
-            let coffOffset = offset
+            offset += 6
+            let numberOfSections = data.extract(UInt16.self, offset: offset)
 
-            offset += 16
+            offset += 14
             let sizeOfOptionalHeader = data.extract(UInt16.self, offset: offset)
 
-            if sizeOfOptionalHeader > 0 {
-                offset = coffOffset + 20
-                let magic = data.extract(UInt16.self, offset: offset)
-                if magic == 0x10b {
-                    // PE32
-                    offset = coffOffset + 112
-                    let resourceTableAddress = data.extract(UInt32.self, offset: offset)
+            offset += Int(sizeOfOptionalHeader) + 4
 
-                    offset += 4
-                    let resourceTableSize = data.extract(UInt32.self, offset: offset)
-
-                    resourceSection = try ResourceSection(data: data,
-                                                          address: resourceTableAddress,
-                                                          size: resourceTableSize)
-                } else if magic == 0x20b {
-                    // PE32+
-                    offset = coffOffset + 128
-                    let resourceTableAddress = data.extract(UInt32.self, offset: offset)
-
-                    offset += 4
-                    let resourceTableSize = data.extract(UInt32.self, offset: offset)
-
-                    resourceSection = try ResourceSection(data: data,
-                                                          address: resourceTableAddress,
-                                                          size: resourceTableSize)
-                } else {
-                    throw("Could not find magic number in Optional Header")
+            for index in 0..<numberOfSections - 1 {
+                if let name = String(data: data[offset..<offset + 8], encoding: .utf8) {
+                    if name.replacingOccurrences(of: "\0", with: "") == ".rsrc" {
+                        print("Hallelujah")
+                    }
                 }
+
+                offset += 0x28
             }
         }
     }
@@ -63,12 +45,23 @@ struct COFFFileHeader: Hashable {
 struct ResourceSection: Hashable {
     init(data: Data, address: UInt32, size: UInt32) throws {
         var offset: Int = Int(address)
-        let numberOfNameEntries = data.extract(UInt16.self, offset: offset)
+        print(address)
+        // let numberOfNameEntries = data.extract(UInt16.self, offset: offset)
 
         offset += 2
-        let numberOfIDEntires = data.extract(UInt16.self, offset: offset)
-        print(numberOfNameEntries)
-        print(numberOfIDEntires)
+        // let numberOfIDEntires = data.extract(UInt16.self, offset: offset)
+
+        offset += 2
+        // let nameOffset = data.extract(UInt32.self, offset: offset)
+        // print(offset)
+
         print("\n")
+    }
+}
+
+extension Int {
+    func printHex() {
+        var hex = String(format: "%02X", self)
+        print("0x\(hex)")
     }
 }
