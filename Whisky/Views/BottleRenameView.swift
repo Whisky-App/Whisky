c129cb5c85012e1ef8988ec32946874dba93cb0c
Mykhaylo Marfeychuk <Mika412@users.noreply.github.com>
2023-06-12 16:32:03 +0100

    Add bottle name validation (#89)
    * Add bottle name validation
    
    * Change view to original size
    
    * Fix line length violation
    
    * Replace NSLocalizedString usage
    
    * Change warning text position
    
    * Invert name validation logic
    
    * Rewrite return message, as enum flags
    
    * Update Localizable.strings
    
    ---------
    
    Co-authored-by: Isaac Marovitz <42140194+IsaacMarovitz@users.noreply.github.com>

--- Whisky/Views/BottleRenameView.swift
+++ Whisky/Views/BottleRenameView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct BottleRenameView: View {
     let bottle: Bottle
     @State var newBottleName: String = ""
+    @State var invalidBottleNameDescription: String = ""
+    @State var isValidBottleName: Bool = true
     @Environment(\.dismiss) var dismiss
 
     var body: some View {
@@ -20,20 +22,35 @@ struct BottleRenameView: View {
                 Spacer()
             }
             Divider()
-            HStack {
+            HStack(alignment: .top) {
                 Text("rename.name")
                 Spacer()
                 TextField("", text: $newBottleName)
+                    .onChange(of: newBottleName) { _ in
+                        isValidBottleName = true
+                    }
                     .frame(width: 180)
             }
             Spacer()
             HStack {
+                Text(invalidBottleNameDescription)
+                    .foregroundColor(.red)
+                    .font(.system(.footnote))
+                    .fixedSize(horizontal: false, vertical: true)
+                    .lineLimit(2)
+                    .frame(height: 30, alignment: .center)
+                    .opacity(isValidBottleName ? 0 : 1)
                 Spacer()
                 Button("create.cancel") {
                     dismiss()
                 }
                 .keyboardShortcut(.cancelAction)
                 Button("rename.rename") {
+                    if case .failure(let failureReason) = BottleVM.shared.isValidBottleName(bottleName: newBottleName) {
+                        isValidBottleName = false
+                        invalidBottleNameDescription = failureReason.description
+                        return
+                    }
                     bottle.rename(newName: newBottleName)
                     dismiss()
                 }
