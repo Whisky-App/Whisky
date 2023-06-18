0452a98e2a1857b2e53eb4c736fda166ef7a0ad8
OHaiiBuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2023-06-18 15:47:29 +0700

    feat: automatically update Wine when Whisky build no. changes (#131)
    * feat: automatically update Wine when Whisky build no. changes
    
    * fix: remove the old Wine copy before applying the new one
    
    * fix: Update GPT install status after Wine updates
    
    * feat: use an info plist key instead of build number

--- Whisky/Utils/WineInstaller.swift
+++ Whisky/Utils/WineInstaller.swift
@@ -8,6 +8,9 @@
 import Foundation
 
 class WineInstaller {
+    // Grab the WineBinaryVersion int from Info.plist
+    static let WineBinaryVersion = Bundle.main.infoDictionary?["WineBinaryVersion"] as? Int ?? 0
+
     static let libraryArchive: URL = (Bundle.main.resourceURL ?? URL(fileURLWithPath: ""))
         .appendingPathComponent("Libraries")
         .appendingPathExtension("tar")
@@ -29,11 +32,30 @@ class WineInstaller {
 
             if !FileManager.default.fileExists(atPath: whiskySupportFolder.path) {
                 try FileManager.default.createDirectory(at: whiskySupportFolder, withIntermediateDirectories: true)
+            } else {
+                // Recreate it
+                try FileManager.default.removeItem(at: whiskySupportFolder)
+                try FileManager.default.createDirectory(at: whiskySupportFolder, withIntermediateDirectories: true)
             }
 
             try Tar.untar(tarBall: libraryArchive, toURL: whiskySupportFolder)
+
+            // Write the binary version to the build_version file
+            let buildVersionFile = libraryFolder.appendingPathComponent("build_version")
+            try String(WineBinaryVersion).write(to: buildVersionFile, atomically: true, encoding: .utf8)
         } catch {
             print("Failed to install Wine: \(error)")
+        }
+    }
+
+    static func updateWine() {
+        // Read the build version from the Wine directory
+        let buildVersionFile = WineInstaller.libraryFolder.appendingPathComponent("build_version")
+        let currentVersion = try? String(contentsOf: buildVersionFile, encoding: .utf8)
+
+        // If the current version is not the same as the binary version, we need to update by calling install again
+        if currentVersion != String(WineBinaryVersion) {
+            installWine()
         }
     }
 }
