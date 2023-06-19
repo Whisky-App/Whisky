8e0c4c8967f0023d82a594702b9756bd36adb40a
Isaac Marovitz <isaacryu@icloud.com>
2023-06-19 21:35:55 +0100

    Return URL

--- Whisky/Utils/WineDownload.swift
+++ Whisky/Utils/WineDownload.swift
@@ -8,32 +8,41 @@
 import Foundation
 
 class WineDownload {
-    static func getLatestWine() async {
+    static func getLatestWineURL() async -> URL? {
         let githubURL = "https://api.github.com/repos/IsaacMarovitz/WhiskyBuilder/actions/artifacts"
         if let artifactsURL = URL(string: githubURL) {
-            URLSession.shared.dataTask(with: URLRequest(url: artifactsURL)) { data, _, error in
-                do {
-                    if error == nil, let data = data {
-                        let decoder = JSONDecoder()
-                        decoder.keyDecodingStrategy = .convertFromSnakeCase
-                        let artifacts: Artifacts = try decoder.decode(Artifacts.self, from: data)
-                        // We gotta turn the URL into a nightly.link URL
-                        if let latest = artifacts.artifacts.first {
-                            var url = latest.url
-                            url.replace("https://api.github.com/repos/", with: "https://nightly.link/")
-                            let selection = "actions/"
-                            if let range = url.range(of: selection) {
-                                url = String(url[..<range.upperBound])
-                                url += "runs/\(latest.workflowRun.id)/\(latest.name).zip"
-                                print(url)
+            return await withCheckedContinuation { continuation in
+                URLSession.shared.dataTask(with: URLRequest(url: artifactsURL)) { data, _, error in
+                    do {
+                        if error == nil, let data = data {
+                            let decoder = JSONDecoder()
+                            decoder.keyDecodingStrategy = .convertFromSnakeCase
+                            let artifacts: Artifacts = try decoder.decode(Artifacts.self, from: data)
+                            // We gotta turn the URL into a nightly.link URL
+                            if let latest = artifacts.artifacts.first {
+                                var url = latest.url
+                                url.replace("https://api.github.com/repos/", with: "https://nightly.link/")
+                                let selection = "actions/"
+                                if let range = url.range(of: selection) {
+                                    url = String(url[..<range.upperBound])
+                                    url += "runs/\(latest.workflowRun.id)/\(latest.name).zip"
+                                    continuation.resume(returning: URL(string: url))
+                                    return
+                                }
                             }
                         }
+                    } catch {
+                        print(error)
                     }
-                } catch {
-                    print(error)
-                }
-            }.resume()
+                    continuation.resume(returning: nil)
+                }.resume()
+            }
         }
+        return nil
+    }
+
+    static func downloadWine(from: URL) async {
+
     }
 }
 
