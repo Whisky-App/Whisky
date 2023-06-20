42863a44c604b86b8404a8c6363a98eb7a06f8b7
Isaac Marovitz <isaacryu@icloud.com>
2023-06-20 09:52:25 +0100

    CPUArch

--- Whisky/Views/Setup Views/WelcomeView.swift
+++ Whisky/Views/Setup Views/WelcomeView.swift
@@ -20,11 +20,13 @@ struct WelcomeView: View {
             .padding(.horizontal)
             Spacer()
             Form {
-                HStack {
-                    Circle()
-                        .foregroundColor(.green)
-                        .frame(width: 10)
-                    Text("Rosetta installed")
+                if cpuArch() == .arm {
+                    HStack {
+                        Circle()
+                            .foregroundColor(.green)
+                            .frame(width: 10)
+                        Text("Rosetta installed")
+                    }
                 }
                 HStack {
                     Circle()
@@ -42,5 +44,35 @@ struct WelcomeView: View {
             .formStyle(.grouped)
             .scrollDisabled(true)
         }
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
+
+enum CPUArch {
+    case arm
+    case intel
+    case unknown
+}
+
+func cpuArch() -> CPUArch {
+    var size: size_t = MemoryLayout<UInt32>.size
+    var type: UInt32 = 0
+
+    sysctlbyname("hw.cputype", &type, &size, nil, 0)
+
+    let arch: Int32 = Int32(type & ~CPU_ARCH_MASK)
+
+    switch arch {
+    case CPU_TYPE_ARM:
+        return .arm
+    case CPU_TYPE_X86_64, CPU_TYPE_X86:
+        return .intel
+    default:
+        return .unknown
     }
 }
