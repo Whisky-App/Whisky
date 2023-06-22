ff6f5b68f915331893f124c36da9dfca64239c1f
JoshuaBrest <36625023+JoshuaBrest@users.noreply.github.com>
2023-06-22 04:49:42 -0700

    Fix bugs

---
+++ Whisky/Views/BottleIcon.swift
@@ -0,0 +1,18 @@
+//
+//  BottleIcon.swift
+//  Whisky
+//
+//  Created by Josh on 6/22/23.
+//
+
+import SwiftUI
+
+struct BottleIcon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BottleIcon()
+}
