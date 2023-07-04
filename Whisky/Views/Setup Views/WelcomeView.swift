28cc6c632cff527ee1be2b4a0d72a363e0193ac5
zhufucdev <1095291434@qq.com>
2023-07-05 00:49:16 +0800

    Ability to reinstall previously installed toolkits

--- Whisky/Views/Setup Views/WelcomeView.swift
+++ Whisky/Views/Setup Views/WelcomeView.swift
@@ -11,6 +11,9 @@ struct WelcomeView: View {
     @State var rosettaInstalled: Bool?
     @State var wineInstalled: Bool?
     @State var gptkInstalled: Bool?
+    @State var rossettaReinstall: Bool = false
+    @State var wineReinstall: Bool = false
+    @State var gptkReinstall: Bool = false
     @State var canContinue: Bool = false
     @Binding var path: [SetupStage]
     @Binding var showSetup: Bool
@@ -30,11 +33,14 @@ struct WelcomeView: View {
             Form {
                 if Arch.getArch() == .arm {
                     InstallStatusView(isInstalled: $rosettaInstalled,
+                                      reinstall: $rossettaReinstall,
                                       name: "Rosetta")
                 }
                 InstallStatusView(isInstalled: $wineInstalled,
+                                  reinstall: $wineReinstall,
                                   name: "Wine")
                 InstallStatusView(isInstalled: $gptkInstalled,
+                                  reinstall: $gptkReinstall,
                                   name: "GPTK")
             }
             .formStyle(.grouped)
@@ -54,23 +60,27 @@ struct WelcomeView: View {
                 }
                 .keyboardShortcut(.cancelAction)
                 Spacer()
+                Button("Cancel") {
+                    showSetup = false
+                }
+                .disabled(rosettaInstalled != true || wineInstalled != true || gptkInstalled != true)
                 Button("Next") {
                     if let rosettaInstalled = rosettaInstalled,
                        let wineInstalled = wineInstalled,
                        let gptkInstalled = gptkInstalled {
                         if Arch.getArch() == .arm {
-                            if !rosettaInstalled {
+                            if !rosettaInstalled || rossettaReinstall {
                                 path.append(.rosetta)
                                 return
                             }
                         }
 
-                        if !wineInstalled {
+                        if !wineInstalled || wineReinstall {
                             path.append(.wineDownload)
                             return
                         }
 
-                        if !gptkInstalled {
+                        if !gptkInstalled || gptkReinstall {
                             path.append(.gptk)
                             return
                         }
@@ -88,6 +98,8 @@ struct WelcomeView: View {
 
 struct InstallStatusView: View {
     @Binding var isInstalled: Bool?
+    @Binding var reinstall: Bool
+    @State var showReinstallBtn: Bool = false
     @State var name: String
     @State var text: String = NSLocalizedString("setup.install.checking",
                                                 comment: "")
@@ -95,7 +107,10 @@ struct InstallStatusView: View {
     var body: some View {
         HStack {
             Group {
-                if let installed = isInstalled {
+                if reinstall {
+                    Circle()
+                        .foregroundColor(.yellow)
+                } else if let installed = isInstalled {
                     Circle()
                         .foregroundColor(installed ? .green : .red)
                 } else {
@@ -105,6 +120,23 @@ struct InstallStatusView: View {
             }
             .frame(width: 10)
             Text(String.init(format: text, name))
+            if showReinstallBtn {
+                Spacer()
+                Button("button.reinstall") {
+                    if !reinstall {
+                        text = NSLocalizedString("setup.install.reinstall", comment: "")
+                    } else {
+                        text = NSLocalizedString("setup.install.installed", comment: "")
+                    }
+                    reinstall = !reinstall
+                }
+            }
+        }
+        .frame(maxWidth: .infinity, minHeight: 20, alignment: .leading)
+        .onHover { hovering in
+            if isInstalled == true {
+                showReinstallBtn = hovering
+            }
         }
         .onChange(of: isInstalled) { _ in
             if let installed = isInstalled {
