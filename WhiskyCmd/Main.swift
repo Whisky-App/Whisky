510204fd35b5a10211aeaed81764536fd4760004
Isaac Marovitz <isaacryu@icloud.com>
2023-08-26 15:38:25 +0100

    Adjustments

--- WhiskyCmd/Main.swift
+++ WhiskyCmd/Main.swift
@@ -12,29 +12,52 @@ import ArgumentParser
 struct Whisky: ParsableCommand {
 	static var configuration = CommandConfiguration(
 		abstract: "A CLI interface for Whisky.",
-		subcommands: [Create.self, Delete.self, Remove.self, Install.self, Uninstall.self])
+		subcommands: [Create.self,
+					  Add.self,
+					  Export.self,
+					  Delete.self,
+					  Remove.self,
+					  Install.self,
+					  Uninstall.self])
 }
 
 extension Whisky {
 	struct Create: ParsableCommand {
 		static var configuration = CommandConfiguration(abstract: "Create a new bottle.")
-		
+
+		mutating func run() throws {
+			print("Create a bottle")
+		}
+	}
+
+	struct Add: ParsableCommand {
+		static var configuration = CommandConfiguration(abstract: "Add an existing bottle.")
+
+		mutating func run() throws {
+			print("Create a bottle")
+		}
+	}
+
+	struct Export: ParsableCommand {
+		static var configuration = CommandConfiguration(abstract: "Export an existing bottle.")
+
 		mutating func run() throws {
 			print("Create a bottle")
 		}
 	}
 
 	struct Delete: ParsableCommand {
-		static var configuration = CommandConfiguration(abstract: "Deletes bottle with given name.")
-		
+		static var configuration = CommandConfiguration(abstract: "Delete a an existing bottle from disk.")
+
 		mutating func run() throws {
 			print("Delete a bottle")
 		}
 	}
 
 	struct Remove: ParsableCommand {
-		static var configuration = CommandConfiguration(abstract: "Removes bottle with given name from Whisky, but does not delete it from disk.")
-		
+		static var configuration = CommandConfiguration(abstract: "Remove an existing bottle from Whisky.",
+														discussion: "Unlike the delete command, this will not remove the bottle from disk.")
+
 		mutating func run() throws {
 			print("Remove a bottle")
 		}
@@ -42,22 +65,22 @@ extension Whisky {
 
 	struct Install: ParsableCommand {
 		static var configuration = CommandConfiguration(abstract: "Install Whisky dependencies.")
-		
+
 		@Flag(name: [.long, .short], help: "Download & Install Wine") var wine = false
 		@Option(name: [.long, .short], help: "Install GPTK from .dmg") var gptk: String?
-		
+
 		mutating func run() throws {
 			print("Install deps")
 		}
 	}
 
 	struct Uninstall: ParsableCommand {
-		static var configuration = CommandConfiguration(abstract: "Uninstall Whisky dependencies.", 
-														discussion: "Uninstalling Wine implicitly uninstall GPTK.")
-		
+		static var configuration = CommandConfiguration(abstract: "Uninstall Whisky dependencies.",
+														discussion: "Uninstalling Wine implicitly uninstalls GPTK.")
+
 		@Flag(name: [.long, .short], help: "Uninstall Wine") var wine = false
 		@Flag(name: [.long, .short], help: "Uninstall GPTK") var gptk = false
-		
+
 		mutating func run() throws {
 			print("Uninstall deps")
 		}
